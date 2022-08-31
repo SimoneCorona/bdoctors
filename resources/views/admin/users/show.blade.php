ace38b983722f48344e8423d7db50ecc09b8c3fc
SimoneCorona <corona.simone93@gmail.com>
2022-08-31 19:43:14 +0200

    modify show.blade.php layout

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -2,34 +2,47 @@
 
 @section('content')
 
-SHOW UTENTE
-<div id="showUtente">
-    <ul>
+<div class="d-flex justify-content-center">
+    <ul class="m-0 p-0">
         @if($user->photo)
-        <li>
-            <img src="{{ asset('storage/' . $user->photo) }}  alt="{{ $user->name }}">
+        <li class="list-unstyled rounded-circle">
+            <img src="{{ asset('storage/' . $user->photo) }}"  alt="{{ $user->name }}" class="rounded-circle">
         </li>
         @else
-        <li>
-            <img src="{{ asset('img/img-not-found.png') }}" alt="img-not-found">
+        <li class="list-unstyled rounded-circle">
+            <img src="{{ asset('img/img-not-found.png') }}" alt="img-not-found" class="rounded-circle">
         </li>
         @endif
-        <li>{{ $user->name}}</li>
-        <li>{{ $user->surname}}</li>
-        <li>
+        <li class="list-unstyled text-center"> <h2>{{ $user->name}} {{ $user->surname}}</h2></li>
+        <li class="list-unstyled text-center rounded-pill bg-primary m-auto text-light" style="width: 20%">
         @foreach($user->specialties as $specialty)
-        {{ $specialty->specialty_name }}{{ $loop->last ? '' : ', ' }}
+            {{ $specialty->specialty_name }}{{ $loop->last ? '' : ', ' }}
         @endforeach
+        </li>
+        <li class="list-unstyled text-center">{{ $user->address }}</li>
+        <li class="list-unstyled text-center">{{ $user->phone_number }}</li>
+        <li class="list-unstyled text-center">{{ $user->email }}</li>
+        <li class="list-unstyled text-center">{{ $user->cv }}</li>
+        <li class="list-unstyled text-center my-3">
+            <h3>Le tue recensioni</h3>
+            <ul class="m-0 p-0">
+                @foreach($user->reviews as $review)
+                    <li class="list-unstyled">
+                        {{ $review->text_review }}
+                    </li>
+                @endforeach
+            </ul>
+        </li>
+        <li class="list-unstyled text-center my-3">
+            <h3>I tuoi messaggi</h3>
+            <ul class="m-0 p-0">
+                @foreach($user->messages as $message)
+                    <li class="list-unstyled" >
+                        {{ $message->text_message }}
+                    </li>
+                @endforeach
+            </ul>
         </li>
     </ul>
 </div>
 @endsection
-
-
-<style>
-
-#showUtente {
-    background-color: red;
-}
-
-</style>
