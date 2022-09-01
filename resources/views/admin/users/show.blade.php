e6634acfa8737d2d91a29b94782633b3f818af69
katy01101011 <katy.91560@gmail.com>
2022-09-01 14:27:46 +0200

    style: show utente loggato

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -2,45 +2,59 @@
 
 @section('content')
 
-<div class="d-flex justify-content-center">
-    <ul class="d-flex flex-column m-0 p-0">
-        @if($user->photo)
-        <li class="avatar list-unstyled">
-            <img src="{{ asset('storage/' . $user->photo) }}"  alt="{{ $user->name }}">
-        </li>
-        @else
-        <li class="avatar list-unstyled">
-            <img src="{{ asset('img/img-not-found.png') }}" alt="img-not-found">
-        </li>
-        @endif
-        <li class="list-unstyled text-center"> <h2>{{ $user->name}} {{ $user->surname}}</h2></li>
-        <li class="list-unstyled text-center text-light">
-            @foreach($user->specialties as $specialty)
-            <span class="rounded-pill bg-primary px-2 mx-1 text-light">
-                {{ $specialty->specialty_name }}
-            </span>
-        @endforeach
-        </li>
-        <li class="list-unstyled text-center">{{ $user->address }}</li>
-        <li class="list-unstyled text-center">{{ $user->phone_number }}</li>
-        <li class="list-unstyled text-center">{{ $user->email }}</li>
-        <li class="list-unstyled text-center">{{ $user->cv }}</li>
-        <li class="list-unstyled text-center my-3">
+<div>
+    <ul class="user d-flex pt-2 py-4">
+        <div class="user-avatar me-5">
+            @if($user->photo)
+            <li class="avatar list-unstyled">
+                <img src="{{ asset('storage/' . $user->photo) }}"  alt="{{ $user->name }}">
+            </li>
+            @else
+            <li class="avatar list-unstyled">
+                <img src="{{ asset('img/img-not-found.png') }}" alt="img-not-found">
+            </li>
+            @endif
+        </div>
+
+        <div class="user-info">
+            <li class="list-unstyled mb-3"> <h2>{{ $user->name }} {{ $user->surname }}</h2></li>
+            <li class="list-unstyled text-light">
+                @foreach($user->specialties as $specialty)
+                <span class="rounded-pill bg-primary px-3 py-1 me-2 text-light">
+                    {{ $specialty->specialty_name }}
+                </span>
+            @endforeach
+            </li>
+            <li class="list-unstyled mt-3">{{ $user->address }}</li>
+            <li class="list-unstyled">{{ $user->phone_number }}</li>
+            <li class="list-unstyled">{{ $user->email }}</li>
+            <li class="list-unstyled">{{ $user->cv }}</li>
+        </div>
+    </ul>
+
+    <ul id="mex-rev" class="container-fluid d-flex">
+        <li class="row col-6 reviews list-unstyled my-3 me-2">
             <h3>Le tue recensioni</h3>
             <ul class="m-0 p-0">
                 @foreach($user->reviews as $review)
-                    <li class="list-unstyled">
-                        {{ $review->text_review }}
+                    <li class="review list-unstyled mb-2">
+                        <div>
+                            {{ $review->text_review }}
+                        </div>
                     </li>
                 @endforeach
             </ul>
         </li>
-        <li class="list-unstyled text-center my-3">
+        <li class="messages row col-6 list-unstyled my-3 ms-2">
             <h3>I tuoi messaggi</h3>
             <ul class="m-0 p-0">
                 @foreach($user->messages as $message)
-                    <li class="list-unstyled" >
-                        {{ $message->text_message }}
+                    <li class="message list-unstyled" >
+                        <div>
+                            {{ $message->text_message }}
+                            <small>Inviato da {{ $message->author }}</small>
+                            <a href="#">{{ $message->email }}</a>
+                        </div>
                     </li>
                 @endforeach
             </ul>
@@ -50,15 +64,63 @@
 @endsection
 
 <style scoped>
+
+    /* Reset */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .user {
+        margin-left: -2rem;
+    }
+
+    #mex-rev {
+        padding-left: 0;
+    }
+
+
+    .user {
+        border-bottom: 1px solid black;
+    }
+
+    /* Foto tonda */
     .avatar {
-        width: 350px;
-        height: 350px;
+        width: 200px;
+        height: 200px;
+        border: 1px solid black;
         border-radius: 50%;
         overflow: hidden;
     }
 
     .avatar img {
+        width: 200px;
+        height: 200px;
         object-fit: cover;
-        translate: -50%;
     }
+
+    /* Sezione messaggi e recensioni ricevute */
+    .reviews, .messages {
+        padding-bottom: 1rem;
+    }
+
+    .message {
+        border-radius: 15px 0 15px 15px;
+        border: 1px solid black;
+        margin-bottom: 3rem;
+    }
+
+    .message div, .review div {
+        padding: .5rem 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    .review {
+        border-radius: 0 15px 15px 15px;
+        border: 1px solid black;
+
+    }
+
+
 </style>
