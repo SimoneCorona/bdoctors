44fa380aa27d59350d77f2de511b249484c7984c
SimoneCorona <corona.simone93@gmail.com>
2022-09-01 13:05:17 +0200

    adjusted layout for single doctor.vue

--- resources/js/pages/SingleDoctor.vue
+++ resources/js/pages/SingleDoctor.vue
@@ -1,18 +1,46 @@
 <template>
-    <div class="container text-center">
-        <h1>{{ user.name }} {{ user.surname }}</h1>
-        <img class="doctor-image" :src="user.photo ? `${user.photo}`: '/img/img-not-found.png'" />
-        <p>{{ user.address }}</p>
-        <p>{{ user.phone_number }}</p>
-        <p>{{ user.email }}</p>
-        <p>{{ user.cv ? user.cv : 'nessun cv'  }}</p>
-        <div v-for="specialty in user.specialties" :key="specialty.specialty_id">
-            <p>{{ specialty.specialty_name }}</p>
-        </div>
-        <div v-for="review in user.reviews" :key="review.id">
-            <p>{{ review.author }}: {{ review.rating }} stelle</p>
-            <p>{{ review.text_review }}</p>
-        </div>
+    <div class="container">
+        <div class="container">
+                <div class="row w-60 bg-danger">
+                    <!-- container img -->
+                    <div class="col p-0">
+                        <img class="doctor-image rounded-circle w-50 h-60" :src="user.photo ? `${user.photo}`: '/img/img-not-found.png'" />
+                    </div>
+
+                    <!-- Wrapper info dottore -->
+                    <div class="col p-0">
+                        <h1>{{ user.name }} {{ user.surname }}</h1>
+                        <div v-for="specialty in user.specialties" :key="specialty.specialty_id">
+                            <p class="rounded-pill bg-primary px-2 mx-1 text-light">{{ specialty.specialty_name }}</p>
+                        </div>
+                        <p><strong>Indirizzo: </strong>{{ user.address }}</p>
+                        <p><strong>Numero di tel.: </strong>{{ user.phone_number }}</p>
+                        <p><strong>Email: </strong>{{ user.email }}</p>
+                        <h4>Curriculum Vitae:</h4>
+                        <p>{{ user.cv ? user.cv : 'nessun cv' }}</p>
+                    </div>
+
+                    <!-- Wrapper invio messaggi e recensioni -->
+                    <div class="col">
+                        <div class="row flex-column bg-success">
+                            <div class="col">
+                                Invia messaggio
+                            </div>
+                            <div class="col">
+                                Invia recensione
+                            </div> 
+                        </div>
+                    </div>    
+                </div>
+
+            <!-- Wrapper recensioni -->
+                <div class="row w-35 flex-column bg-warning">
+                    <div v-for="review in user.reviews" :key="review.id">
+                        <p>{{ review.author }}: {{ review.rating }} stelle</p>
+                        <p>{{ review.text_review }}</p>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 
