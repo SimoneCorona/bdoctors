f96ecb8b9a27981fd5aeff8c2f7a5a562f3c0d49
katy01101011 <katy.91560@gmail.com>
2022-09-11 13:03:40 +0200

    fix: login e register in italiano

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Benvenuto nuovo utente! Registrati qui compilando i campi, oppure se sei già registrato fai login.') }}</div>
+                <div class="card-header">{{ __('Benvenuto nuovo utente! Registrati qui compilando i campi, oppure se sei già registrato, accedi.') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
